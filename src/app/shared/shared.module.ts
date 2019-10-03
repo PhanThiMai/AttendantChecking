fd7df7c962f1e9aaa668c0638b8d4d43a9f79e88
Phan Thi Mai <1612369@student.hcmus.edu.vn>
2019-10-03 18:54:22 +0700

    Change version of package

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,19 +2,19 @@ import { NgModule, ModuleWithProviders } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { TabsModule,TooltipModule } from 'ngx-bootstrap';
+import { TabsModule, TooltipModule } from 'ngx-bootstrap';
 import { QRCodeModule } from 'angular2-qrcode';
-import {TranslateModule} from '@ngx-translate/core';
+import { TranslateModule } from '@ngx-translate/core';
 import { LocalStorageModule } from 'angular-2-local-storage';
-import {FileUploadModule} from "ng2-file-upload";
+import { FileUploadModule } from "ng2-file-upload";
 import { AppConfig } from './config'
-export {AppConfig};
+export { AppConfig };
 
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
 import { TopNavigationComponent } from './components/top-navigation/top-navigation.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { PageNotFoundComponent } from './components/page-not-found.component';
-export {PageNotFoundComponent};
+export { PageNotFoundComponent };
 import { EditScheduleModalComponent } from './components/edit-schedule-modal/edit-schedule-modal.component';
 export { EditScheduleModalComponent };
 import { ResultMessageModalComponent } from './components/result-message-modal/result-message-modal.component';
@@ -31,42 +31,42 @@ import { QRCodeComponent } from './components/qr-code.component';
 export { QRCodeComponent };
 import { QuizDisplayComponent } from './components/quiz-display/quiz-display.component';
 export { QuizDisplayComponent };
-import {AppService} from './services/app.service';
-export {AppService};
-import {CourseService} from './services/courses.service';
-export {CourseService};
-import {TeacherService} from './services/teachers.service';
-export {TeacherService};
-import {AttendanceService} from './services/attendance.service';
-export {AttendanceService};
-import {ExcelService} from './services/excel.service';
-export {ExcelService};
-import {ScheduleService} from './services/schedule.service';
-export {ScheduleService};
-import {StudentService} from './services/student.service';
-export {StudentService};
-import {AbsenceRequestService} from './services/absence-request.service';
-export {AbsenceRequestService};
-import {AuthGuardService} from './services/auth-guard.service';
-export {AuthGuardService};
-import {AuthService} from './services/auth.service';
-export {AuthService};
-import {SemesterService} from './services/semester.service';
-export {SemesterService};
-import {FeedbackService} from './services/feedback.service';
-export {FeedbackService};
-import {SocketService} from './services/socket.service';
-export {SocketService};
-import {CheckAttendanceService} from './services/check-attendance.service';
-export {CheckAttendanceService};
-import {QuizService} from './services/quiz.service';
-export {QuizService};
-import {ClassesService} from './services/classes.service';
-export {ClassesService};
-import {ProgramsService} from './services/programs.service';
-export {ProgramsService};
-import {NotificationService} from './services/notification.service';
-export {NotificationService};
+import { AppService } from './services/app.service';
+export { AppService };
+import { CourseService } from './services/courses.service';
+export { CourseService };
+import { TeacherService } from './services/teachers.service';
+export { TeacherService };
+import { AttendanceService } from './services/attendance.service';
+export { AttendanceService };
+import { ExcelService } from './services/excel.service';
+export { ExcelService };
+import { ScheduleService } from './services/schedule.service';
+export { ScheduleService };
+import { StudentService } from './services/student.service';
+export { StudentService };
+import { AbsenceRequestService } from './services/absence-request.service';
+export { AbsenceRequestService };
+import { AuthGuardService } from './services/auth-guard.service';
+export { AuthGuardService };
+import { AuthService } from './services/auth.service';
+export { AuthService };
+import { SemesterService } from './services/semester.service';
+export { SemesterService };
+import { FeedbackService } from './services/feedback.service';
+export { FeedbackService };
+import { SocketService } from './services/socket.service';
+export { SocketService };
+import { CheckAttendanceService } from './services/check-attendance.service';
+export { CheckAttendanceService };
+import { QuizService } from './services/quiz.service';
+export { QuizService };
+import { ClassesService } from './services/classes.service';
+export { ClassesService };
+import { ProgramsService } from './services/programs.service';
+export { ProgramsService };
+import { NotificationService } from './services/notification.service';
+export { NotificationService };
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded module.
  */
@@ -79,9 +79,9 @@ export {NotificationService};
     TabsModule,
     QRCodeModule,
     LocalStorageModule.withConfig({
-            prefix: 'qldd',
-            storageType: 'localStorage'
-        }),
+      prefix: 'qldd',
+      storageType: 'localStorage'
+    }),
     FileUploadModule,
     TooltipModule.forRoot(),
     TranslateModule
@@ -114,7 +114,7 @@ export {NotificationService};
     QRCodeComponent,
     QuizDisplayComponent,
     TranslateModule
-  ],  
+  ],
   providers: [
     AppService,
     CourseService,
@@ -136,4 +136,4 @@ export {NotificationService};
     NotificationService
   ]
 })
-export class SharedModule {}
+export class SharedModule { }
